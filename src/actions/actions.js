d3c68c0011de49b984bddf1a85406c0a11a162c2
Joseph Case <joseph.case@hotmail.co.uk>
2018-01-11 09:14:41 +0100

    added navigation animation design

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -4,7 +4,6 @@ export const CHANGE_PAGE = 'CHANGE_PAGE';
 export const CHANGE_LANGUAGE = 'CHANGE_LANGUAGE';
 export const TOGGLE_SHOWHIDE_MENU = 'TOGGLE_SHOWHIDE_MENU';
 export const PAGE_NOT_FOUND = 'PAGE_NOT_FOUND';
-export const FIX_HEADER = 'FIX_HEADER';
 
 export function fetchState(path) {
 
@@ -36,32 +35,35 @@ export function navigate(path) {
 
 	return (dispatch, getState) => {
 
-		dispatch(changePageStart(path));
-
 		if(window) {
 			var doc = document.documentElement;
 			let top = (window.pageYOffset || doc.scrollTop)  - (doc.clientTop || 0);
 			if(top < (window.innerHeight - 50)) {
 				window.scroll({
-					top: (window.innerHeight - 49), 
+					top: (window.innerHeight - 50), 
 					left: 0, 
 					behavior: 'smooth' 
 				})
 			}
 		}
 
-		setTimeout(() => {
-			if(window) {
-				window.scroll({
-					top: (window.innerHeight - 50)
-				})
-			}
-			if(pageExists(getState(), path)) {
-				return dispatch(changePage(path));
-			} else {
-				return dispatch(pageNotFound(path));
-			}
-		}, 500);
+		if(path != getState().activePage.path) {
+
+			dispatch(changePageStart(path));
+
+			setTimeout(() => {
+				if(window) {
+					window.scroll({
+						top: (window.innerHeight - 50)
+					})
+				}
+				if(pageExists(getState(), path)) {
+					return dispatch(changePage(path));
+				} else {
+					return dispatch(pageNotFound(path));
+				}
+			}, 300);
+		}
 	}
 
 }
--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 
 import Header from './Header.js';
@@ -8,47 +8,61 @@ import SectionContent from './SectionContent.js';
 import PageNotFound from './PageNotFound.js';
 
 
-const MainContent = (props) => {
+class MainContent extends Component {
 
-	let {error, type, pages, content, pageChanging, path} = props;
+	constructor(props) {
+		super(props);
+		this.state = { isNavigating: true };
+	}
 
-	const classes = ['App'];
-	classes.push(pageChanging ? 'isNavigating' : '');
-
-	var activeContent;
-	if(error) {
-		activeContent = <PageNotFound />
-	} else {
-		switch(type) {
-			case 'section':
-				activeContent = <SectionContent pages={pages} />
-				break;
-			case 'page':
-				activeContent = <PageContent content={content} />
-				break;
-			default:
-				activeContent = null
+	componentDidUpdate() {
+		if(this.state.isNavigating !== this.props.pageChanging) {
+			this.setState({isNavigating: this.props.pageChanging})
 		}
 	}
 
-	return (
-		<div className={classes.join(' ')} data-path={path}>
-			<div className='loader'></div>
-			<div className='landingPage'>
-				<video autoPlay='true' loop='true'>
-					<source src='http://www.giusypirrotta.com/content/file_241.webm' type='video/webm' />
-					<source src='http://www.giusypirrotta.com/content/file_241.mp4' type='video/mp4' />
-				</video>
-				<h1>Giusy Pirrotta</h1>
-			</div>
-	        <Header />
-	        <div className='contentContainer'>
-				{ activeContent }
-	        </div>
-	        <Footer />
-			<div className='fader'></div>
-     	</div>
-	)
+	render() {
+
+		let {error, type, pages, content, path} = this.props;
+
+		const classes = ['App'];
+		classes.push(this.state.isNavigating ? 'isNavigating' : '');
+
+		var activeContent;
+		if(error) {
+			activeContent = <PageNotFound />
+		} else {
+			switch(type) {
+				case 'section':
+					activeContent = <SectionContent pages={pages} />
+					break;
+				case 'page':
+					activeContent = <PageContent content={content} />
+					break;
+				default:
+					activeContent = null
+			}
+		}
+
+		return (
+			<div className={classes.join(' ')} data-path={path}>
+				<div className='loader'></div>
+				<div className='landingPage'>
+					<video autoPlay='true' loop='true'>
+						<source src='http://www.giusypirrotta.com/content/file_241.webm' type='video/webm' />
+						<source src='http://www.giusypirrotta.com/content/file_241.mp4' type='video/mp4' />
+					</video>
+					<h1>Giusy Pirrotta</h1>
+				</div>
+		        <Header />
+		        <div className='contentContainer'>
+					{ activeContent }
+		        </div>
+		        <Footer />
+				<div className='fader'></div>
+	     	</div>
+		)
+	}
 }
 
 export default MainContent;

af7df9d3ba1110c30ff29835d0089956c712d75f
Joseph Case <joseph.case@hotmail.co.uk>
2018-01-17 00:39:23 +0100

    changed video functionality

--- src/components/InlineVideo.js
+++ src/components/InlineVideo.js
@@ -4,36 +4,30 @@ class InlineVideo extends PureComponent {
 
 	constructor(props) {
 		super(props);
-		this.handleError = this.handleError.bind(this);
-		this.state = { useVideo: true };		
+		this.playHandler = this.playHandler.bind(this);
+		this.state = { showVideo: false };		
 	}
 
-	handleError() {
-		this.setState({useVideo: false});
+	playHandler() {
+		this.setState({showVideo: true});
 	}
 
 	render() {
 		let { children, fallbackImage, ...otherProps } = this.props;
-		if(this.state.useVideo) {
-			let childrenLength = children.length
-			return(
-				<video {...otherProps} autoPlay='true' loop='true' muted='true' playsInline='true' webkit-playsinline='true'>
-					{ 
-						children.map((child, i) => {
-							if(i === childrenLength - 1) {
-								return React.cloneElement(child, { key: i, onError: this.handleError });
-							}
-							return React.cloneElement(child, { key: i });;
-						}) 
-					}
-				</video>
-			)
-		} else {
-			let divStyle = {
-				backgroundImage: `url(${fallbackImage})`,
-			};
-			return <div {...otherProps} style={divStyle}></div>
-		}
+		let className = this.state.showVideo ? 'show' : 'hide';
+		return(
+			<video 
+				className={className} 
+				autoPlay='true' 
+				loop='true'
+				muted='true'
+				playsInline='true'
+				webkit-playsinline='true'
+				onPlay={this.playHandler}
+			>
+				{children}
+			</video>
+		)
 	}
 }
 

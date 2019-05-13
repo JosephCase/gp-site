dd5a2ab6f7592a23374aa0fff64c7a6d3adf3e5c
EC2 Default User <ec2-user@ip-172-31-47-1.eu-west-1.compute.internal>
2019-05-13 19:23:49 +0000

    changed logic to always use highest quality image. enabled user zoom

--- server/view.js
+++ server/view.js
@@ -12,7 +12,7 @@ exports.render = (html, state) => {
 
 		<head>
 			<meta charset="utf-8">
-			<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+			<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
 			<meta property='og:image' content='http://www.giusypirrotta.com/shareImg.jpg'/>
 				
--- src/config.js
+++ src/config.js
@@ -4,7 +4,8 @@ const config = {
 	contentHost: '/content',
 	PAGE: 'page',
 	SECTION: 'section',
-	imageSizes: [1400, 1000, 700, 500]
+	// imageSizes: [1400, 1000, 700, 500]
+	imageSizes: [1400]
 }
 
 export default config;
